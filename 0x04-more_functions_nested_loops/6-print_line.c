c76cfae9831a3215e05922dc72a3cb955e946902
Rahab Wanyiri <rahabsheshi22@gmail.com>
2022-03-17 22:06:06 +0000

    print lines with characters

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* desc: print_line-draws a straight line using character _
+* @n:The number of _ characters to be printed
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+int line;
+if (n > 0)
+{
+for (line = 0; line < n; line++)
+_putchar('_');
+}
+_putchar('\n');
+}
