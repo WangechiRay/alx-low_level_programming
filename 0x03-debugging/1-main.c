8493baf98769a7598a2e9fb81610ba423b4a6bf0
Rahab Wanyiri <rahabsheshi22@gmail.com>
2022-03-18 00:22:11 +0000

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 int i;
@@ -19,9 +18,7 @@ i = 0;
 * putchar(i);
 * }
 */
-
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
 
