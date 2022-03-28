740365e40c989769024589473e3a5b462088bbad
Rahab Wanyiri <rahabsheshi22@gmail.com>
2022-03-28 18:32:40 +0000

    looking for character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,8 @@
 * _strchr- locates a character in a string
 * @s:first character to be located.
 * @c:second character to be located in a string
-* Return: first occurence of character or NULL if not found
+* Return: first occurence of character in @c string
+* @s. Return NULL if the character isn't found.
 */
 
 char *_strchr(char *s, char c)
