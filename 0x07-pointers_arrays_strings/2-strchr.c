9a4b10f4dde0321bbcd307802ed90e1669390bf9
Rahab Wanyiri <rahabsheshi22@gmail.com>
2022-03-28 21:17:59 +0000

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,8 +3,10 @@
 
 /**
 * _strchr- locates a character in a string
+*
 * @s:first character to be located.
 * @c:second character to be located in a string
+*
 * Return: first occurence of character in @c string
 * @s. Return NULL if the character isn't found.
 */
