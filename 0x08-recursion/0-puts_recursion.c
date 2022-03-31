0c3ee0554b0bf3baa2f9286450ab3c0d3ec3a90c
wangechi <rahabsheshi22@gmail.com>
2022-03-31 20:38:40 +0000

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * void _puts_recursion-function that prints a string.
+ * @s:character to be printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar('\n');
+}
+}
