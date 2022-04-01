1649733c7657245f1925dd7b0ad892f7de92d0dd
wangechi <rahabsheshi22@gmail.com>
2022-04-01 17:02:36 +0000

    prints number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -14,12 +14,12 @@ int main(int argc, char *argv[])
 int i;
 
 if (argc == 1)
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
 else
 {
-for(i = 0; *argv; i++, argv++)
+for (i = 0; *argv; i++, argv++)
 ;
 printf("%d\n", i - 1);
 }
-return 0;
+return (0);
 }
