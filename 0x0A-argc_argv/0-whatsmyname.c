926ef8ad9b0634f28875e3f7fcac00e5065500a1
wangechi <rahabsheshi22@gmail.com>
2022-04-01 15:51:29 +0000

    printing name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main-prints its name
+* @argc:counts number of arguments
+* @argv:size of the argc
+*
+* Return:0
+*/
+
+int main(int argc, char * argv[])
+{
+printf("%s\n", argv[argc * 0]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+rning how to use arguments passed to the program and the two main arguments used which are argc and argv
