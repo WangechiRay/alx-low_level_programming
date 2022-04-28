a893541526b920a49804e875585b240807e7ec50
wangechi <rahabsheshi22@gmail.com>
2022-04-28 21:25:07 +0000

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,19 +12,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int len = 0;
-	
-	if (b[len] == '\0')
+	int i;
+	unsigned int dec_val = 0;
+
+	if (!b)
 		return (0);
 
-	while ((b[len] == '0') || (b[len]  == '1'))
+	for (i = 0; b[i]; i++)
 	{
-		n <<= 1;
-		n += b[len] - '0';
-		len++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (n);
+	return (dec_val);
 }
 
