14fb2e9949d6a800c7f4f517e293673ef239df39
wangechi <rahabsheshi22@gmail.com>
2022-06-10 01:18:51 +0000

    length of list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -12,12 +12,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t t = 0;
+	size_t t = 0;
 
-        while (h != NULL)
-        {
-                t++;
-                h = h->next;
-        }
-        return (t);
+	while (h != NULL)
+	{
+		t++;
+		h = h->next;
+	}
+	return (t);
 }
